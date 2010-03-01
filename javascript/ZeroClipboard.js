03fe6e5be796883365c6a90a3018aa226dd7c6c9
Paul Irish <paul.irish@gmail.com>
2010-03-01 03:30:36 +0000

    clipboard works.

--- javascript/ZeroClipboard.js
+++ javascript/ZeroClipboard.js
@@ -5,7 +5,7 @@ var ZeroClipboard = {
 	
 	version: "1.0.6",
 	clients: {}, // registered upload clients on page, indexed by id
-	moviePath: 'ZeroClipboard.swf', // URL to movie
+	moviePath: 'javascript/ZeroClipboard.swf', // URL to movie
 	nextId: 1, // ID of next movie
 	
 	$: function(thingy) {
