55961db1b9397052fc7c6ae25b344591d048b4bb
Paul Irish <paul.irish@gmail.com>
2011-10-18 22:44:49 -0700

    finish off working demo for animations.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -252,7 +252,7 @@ window.generator = {
 			innerHTML = innerHTML.replace(/{((\n|.)+)}/, '{<span class="declaration-block">$1</span>}');
 			innerHTML = innerHTML.replace(/(.*?) {/, '<span class="selector">$1</span> <span class="bracket">{</span>');
 			// this next one matches the triple line @font-face rule
-			innerHTML = innerHTML.replace(/(.*?):\s?(.*?\n.*?\n.*);/mg, '<span class="declaration"><span class="property">$1</span>: <span class="value">$2</span>;</span>');
+			innerHTML = innerHTML.replace(/(.*?):\s?((?!hover).*?\n.*?\n.*);/mg, '<span class="declaration"><span class="property">$1</span>: <span class="value">$2</span>;</span>');
 			innerHTML = innerHTML.replace(/(.*?):\s?(.*?);/mg, '<span class="declaration"><span class="property">$1</span>: <span class="value">$2</span>;</span>');
 			innerHTML = innerHTML.replace(/}/m, '<span class="bracket">}</span>');
 
