3537218ede5d4013155b86494ae9da2e29ef544c
Mathias Bynens <mathias@qiwi.be>
2012-01-14 18:04:51 +0100

    Minor JavaScript cleanup

--- javascript/cb_plugins.js
+++ javascript/cb_plugins.js
@@ -1,20 +1,17 @@
-// remap jQuery to $
-(function($){
-$(document).ready(function(){
+$(function(){
 
+	var $toggle = $('#toggle_style');
 
 	/*==TOGGLE STYLING==*/
 	$('#sandboxwrap .off').click().toggle(
 		// Change to my styling
-		function(){
-			$('#toggle_style').html('<link rel="stylesheet" href="css/light.css" type="text/css" media="screen" />');
+		function() {
+			$toggle.html('<link rel="stylesheet" href="css/light.css" />');
 		},
 		// ...and back to original CSS3, please! styling.
-		function(){
-			$('#toggle_style *').remove();
+		function() {
+			$toggle.empty();
 		}
 	);
 
-
 });
-})(window.jQuery);
